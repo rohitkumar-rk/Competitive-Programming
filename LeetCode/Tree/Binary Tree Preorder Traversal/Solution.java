9430fdcf8639aba509b1ade4a71f81d7fcac2fae
Rohit Kumar <rohitdpskh@gmail.com>
2020-05-25 23:25:56 +0530

    Update : Fixed preorderTraversal Function

--- LeetCode/Tree/Binary Tree Preorder Traversal/Solution.java
+++ LeetCode/Tree/Binary Tree Preorder Traversal/Solution.java
@@ -15,7 +15,6 @@
  */
 class Solution {
     
-    //Recursive approach
     public List<Integer> preorderHelper(TreeNode root, List<Integer> res){
         if(root == null)
             return res;
@@ -28,8 +27,7 @@ class Solution {
     
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        while(){
-            
-        }
+        res = preorderHelper(root, res);
+        return res;
     }
 }
