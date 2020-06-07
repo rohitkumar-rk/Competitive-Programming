2bb7086a39525a364b6b62724aaecfbae36a4259
Rohit Kumar <rohitdpskh@gmail.com>
2020-06-07 21:41:58 +0530

    Added : Reverse String

---
+++ LeetCode/June Challenge/4. Reverse String/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        
+    }
+}
