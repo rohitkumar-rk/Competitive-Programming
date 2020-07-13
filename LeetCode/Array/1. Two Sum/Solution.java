9a9d259b6227c90526bf9cb9924e363c083614cf
Rohit Kumar <rohitdpskh@gmail.com>
2020-07-13 21:10:03 +0530

    Added : Two Sum

---
+++ LeetCode/Array/1. Two Sum/Solution.java
@@ -0,0 +1,23 @@
+/*
+@lc id : 1
+@problem : Two Sum
+@author : rohit
+@url : https://leetcode.com/problems/two-sum/
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+        
+            if( map.containsKey( target - nums[i] )
+             &&  map.get(target - nums[i]) != i)
+                return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+        
+        return null;
+    }
+}
