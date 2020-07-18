5137ed8d9a63efcd642d0d94590d52b110e2d01d
Rohit Kumar <rohitdpskh@gmail.com>
2020-07-18 16:55:09 +0530

    Added : Insert Delete GetRandom O(1)

--- LeetCode/Array/287. Find the Duplicate Number/readme.md
+++ LeetCode/Array/287. Find the Duplicate Number/readme.md
@@ -8,6 +8,7 @@ Input: [1,3,4,2,2]
 Output: 2
 ```
 Example 2:
+```
 Input: [3,1,3,4,2]
 Output: 3
 ```
---
+++ LeetCode/Array/380. Insert Delete GetRandom O(1)/Solution.java
@@ -0,0 +1,52 @@
+/**
+
+@lc id : 380
+@problem : Insert Delete GetRandom O(1)
+@author : github.com/rohitkumar-rk
+@url : https://leetcode.com/problems/insert-delete-getrandom-o1/
+@difficulty : medium
+*/
+
+class RandomizedSet {
+    
+    HashSet<Integer> set;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        set  = new HashSet<Integer>();
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(set.contains(val))
+            return false;
+        set.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(set.contains(val)){
+            set.remove(val);
+            return true;
+        }
+        return false; 
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<Integer>(set);
+        int pos = random.nextInt(list.size());
+        return list.get(pos);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
