434dc3ef05e1c186a2a33c61f5d630e795195530
Rohit Kumar <rohitdpskh@gmail.com>
2020-07-19 22:21:42 +0530

    ADded : Square of a Sorted Array

---
+++ LeetCode/Array/977. Squares of a Sorted Array/Solution.java
@@ -0,0 +1,21 @@
+/**
+
+@lc id : 977
+@problem : Squares of a Sorted Array
+@author : github.com/rohitkumar-rk
+@url : https://leetcode.com/problems/squares-of-a-sorted-array/
+@difficulty : easy 
+*/
+
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        
+        for(int i = 0; i < A.length; i++){
+            A[i] = A[i] * A[i];
+        }
+        
+        Arrays.sort(A);
+        return A;
+    }
+}
