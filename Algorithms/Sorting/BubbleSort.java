4ec26d7b038b86a895f117f109da4fb8d7b06280
Rohit Kumar <rohitdpskh@gmail.com>
2020-08-18 18:10:26 +0530

    Update BubbleSort.java

--- Algorithms/Sorting/BubbleSort.java
+++ Algorithms/Sorting/BubbleSort.java
@@ -44,8 +44,8 @@ public class BubbleSort {
 
 /*
 Time Complexity : Best Case - O(n)
-				  Average Case - O(n^2)
-				  Worst Case - O(n^2)
+		  Average Case - O(n^2)
+	          Worst Case - O(n^2)
 
 Space Complexity : O(1)
 
