efb339e42025e6a167d6ebfc55827f94e85332e2
Rohit Kumar <rohitdpskh@gmail.com>
2020-08-18 18:06:56 +0530

    Updated : BubbleSort

--- Algorithms/Sorting/BubbleSort.java
+++ Algorithms/Sorting/BubbleSort.java
@@ -3,16 +3,29 @@ public class BubbleSort {
 
 	public static void bubbleSort(int[] input){
 
+		boolean swapped;
 		for(int i = 0; i < input.length - 1; i++){
-			for(int j = 0; j < input.length - 1; j++){
+			swapped = false;
+			/*We don't need to go to end of array on each traversal
+			*because on each travelsal, one element is placed at its
+			*correct position towards end of array
+			So we go till length - i -1 */
+
+			for(int j = 0; j < input.length - i - 1; j++){
 				//If jth element is greater than (j+1)th
 				//Swap these
 				if(input[j] > input[j+1]){
 					int temp = input[j];
 					input[j] = input[j+1];
 					input[j+1] = temp;
+					swapped = true;
 				}
 			}
+
+			/*If no two elements were swapped by inner loop,
+			the array is in sorted order, so break */
+			if(swapped == false)
+				break;
 		}
 
 	}
@@ -28,3 +41,14 @@ public class BubbleSort {
 	}
 
 }
+
+/*
+Time Complexity : Best Case - O(n)
+				  Average Case - O(n^2)
+				  Worst Case - O(n^2)
+
+Space Complexity : O(1)
+
+Note : boolean swapped is added to it to optimize it
+to break when the array is in sorted order
+*/
